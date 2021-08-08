e60b5b337e6c7b24e0b241492fadb719235b4171
Harsh Mittal <harshmittal1750@gmail.com>
2021-08-09 00:39:22 +0530

    pushed with some erros to be fixed

--- lib/screen/home_page.dart
+++ lib/screen/home_page.dart
@@ -31,6 +31,13 @@ class _HomePageState extends State<HomePage> {
                 ],
               ),
             ),
+            Expanded(
+              child: (Container(
+                child: (ListView.builder(
+                  itemBuilder: (context, index) => Post(),
+                )),
+              )),
+            ),
             Container(
               child: Post(),
             ),
